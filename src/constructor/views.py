4fb33a52c925e5267571f500110c6e4c459b4690
Evgeniy Kiyko <nataniel1987@gmail.com>
2023-05-14 13:20:14 +0300

    feat: в конструктор добавлено отображение разделов тестов: даты, блоки

--- src/constructor/views.py
+++ src/constructor/views.py
@@ -1,10 +1,12 @@
+from itertools import groupby
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
 from quizzing.models import Quiz
 from .forms import CreateQuizForm, LoginCreateQuizForm
-from .models import Question
+from .models import Question, Category
 
 
 def create_quiz(request):
@@ -18,9 +20,15 @@ def create_quiz(request):
             return HttpResponseRedirect(reverse('constructor:create_quiz'))
     else:
         form = CreateQuizForm()
+    quizzes = Quiz.objects.values('pk', 'username', 'created_at')
+    for quiz in quizzes:
+        quiz['categories'] = [category for category in Category.objects.filter(
+            subcategories__questions__quizzes__pk=quiz['pk']).distinct().values_list('name', flat=True)]
+        quiz['created_at'] = quiz['created_at'].strftime('%Y.%m.%d')
+    quizzes = [(k, list(g)) for k, g in groupby(quizzes, key=lambda x: x['created_at'])]
     return render(request, 'constructor/create_quiz.html', {
         'form': form,
-        'quizzes': Quiz.objects.values_list('pk', 'username'),
+        'quizzes': quizzes,
     })
 
 
